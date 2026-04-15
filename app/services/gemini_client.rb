efb429c7313a4a61a3cc5e10bd3de5a0aec43a0a
morimori-mochimochi <reveluv.chino1205@icloud.com>
2026-04-15 11:47:22 +0900

    gemini_client.rbを修正

--- app/services/gemini_client.rb
+++ app/services/gemini_client.rb
@@ -1,46 +1,42 @@
 class GeminiClient
-  def initialize
-    api_key = ENV['GEMINI_API_KEY']
-    @client = Google::GenerativeAI.new(api_key: api_key)
-  end
 
-  def suggest_recipe(ingredients)
-    return nil if ingredients.blank?
+  require "net/http"
+  require "json"
+  require "uri"
 
-    prompt = build_prompt(ingredients)
-    
-    begin
-      # モデルを指定（gemini-1.5-flashが高速かつ安価で推奨されます）
-      model = @client.model("gemini-1.5-flash")
-      response = model.generate_content(prompt)
-      
-      # レスポンスからテキストを抽出
-      response.text
-    rescue => e
-      Rails.logger.error "Gemini API Error: #{e.message}"
-      nil
-    end
-  end
+  API_KEY = Rails.application.credentials.dig(:google_ai, :api_keys)
 
-  private
+  uri = URI("https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=#{API_KEY}")
 
-  # ここでプロンプトのチューニング（プロンプトエンジニアリング）を行う
-  def build_prompt(ingredients)
-    <<~PROMPT
-      あなたはプロの料理研究家です。以下の材料を使って、家庭で簡単に作れる美味しいレシピを1つ提案してください。
-      材料: #{ingredients}
-      以下のJSON形式で、キーや値の型も完全に守って応答してください。
-      {
-        "recipeName": "料理名",
-        "description": "料理の説明",
-        "ingredients": [
-          { "name": "材料名", "quantity": "分量" }
-        ],
-        "instructions": [
-           "手順1",
-           "手順2"
-        ]
-      }
-    PROMPT
-  end
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+
+  request = Net::HTTP::Post.new(uri)
+  request["Content-Type"] = "application/json"
+
+  request.body = {
+   contents: [
+     {
+       prompt: [
+         <<~PROMPT
+           あなたはプロの料理研究家です。以下の材料を使って、家庭で簡単に作れる美味しいレシピを1つ提案してください。
+           材料: #{ingredients}
+           以下のJSON形式で、キーや値の型も完全に守って応答してください。
+           {
+             "recipeName": "料理名",
+             "description": "料理の説明",
+             "ingredients": [
+               { "name": "材料名", "quantity": "分量" }
+             ],
+             "instructions": [
+                "手順1",
+                "手順2"
+             ]
+           }
+         PROMPT
+       ],
+  response = http.request(request)
+  json = JSON.parse(response.body)
+
+  puts json.dig("candidates", 0, "content", "parts", 0, "text")
 end
